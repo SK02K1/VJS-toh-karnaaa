42ea67ec7548163232b0c2a244bc5888975f5411
SK02K1 <ksourabh458@gmail.com>
2021-12-19 00:12:14 +0530

    style: layout updated

--- app.js
+++ app.js
@@ -1,72 +1,78 @@
-// const formAddNewTask = document.querySelector("#form-add-new-task");
-// const taskContainer = document.querySelector(".task-container");
+const formAddNewTask = document.querySelector("#form-add-new-task");
+const taskContainer = document.querySelector(".task-container");
 const dateContainer = document.querySelector(".date");
-dateContainer.textContent = `${dateFns.format(new Date(), "MMMM d, YYYY")}`
-// class Task {
-//     constructor(task){
-//         this.task = task;
-//         this.isCompelted = false;
-//     }
-// }
 
-// const updateLocalStorage = (newTaskData) => {
-//     const prevTasks = JSON.parse(localStorage.getItem("tasks"));
-//     prevTasks.push(newTaskData);
-//     localStorage.setItem("tasks", JSON.stringify(prevTasks));
-// };
+dateContainer.textContent = `${dateFns.format(new Date(), "MMMM D, YYYY")}`;
+class Task {
+    constructor(task){
+        this.task = task;
+        this.isCompelted = false;
+    }
+}
 
-// const addNewTask = (newTaskData, id) => {
-//     taskContainer.innerHTML += `
-//         <div class="task-card" data-id="${id}">
-//         <span class="task ${newTaskData.isCompelted ? "completed" : ""}">${newTaskData.task}</span>
-//         <div class="controls-container" >
-//             <i class="fas fa-check-square btn-check control"></i>
-//             <i class="fas fa-trash btn-delete control"></i>
-//         </div>
-//         </div>
-//     `;
-// };
+const updateLocalStorage = (newTaskData) => {
+    const prevTasks = JSON.parse(localStorage.getItem("tasks"));
+    prevTasks.push(newTaskData);
+    localStorage.setItem("tasks", JSON.stringify(prevTasks));
+};
 
-// formAddNewTask.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     const tasks = JSON.parse(localStorage.tasks);
-//     const id = tasks.length === 0 ? 0 : tasks.length;
-//     const task = formAddNewTask.task.value.trim();
-//     formAddNewTask.reset();
-//     const newTask = new Task(task);
-//     addNewTask(newTask, id);
-//     updateLocalStorage(newTask);
-// });
+const addNewTask = (newTaskData, id) => {
+    taskContainer.innerHTML += `
+        <div class="task-card" data-id="${id}">
+            <label class="task">
+                <input class="btn-check" type="checkbox" ${newTaskData.isCompelted && "checked"}>
+                <span class="task ${newTaskData.isCompelted ? "completed" : ""}">${newTaskData.task}</span>
+            </label>
+            <div class="controls-container" >
+                <i class="fas fa-trash btn-delete control"></i>
+            </div>
+        </div>
+    `;
+};
 
-// const changeTaskState = (task, id) => {
-//     const tasks = JSON.parse(localStorage.getItem("tasks"));
-//     tasks[id].isCompelted = !(tasks[id].isCompelted);
-//     localStorage.setItem("tasks", JSON.stringify(tasks));
-//     task.classList.toggle("completed");
-// };
+formAddNewTask.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const tasks = JSON.parse(localStorage.tasks);
+    const id = tasks.length === 0 ? 0 : tasks.length;
+    const task = formAddNewTask.task.value.trim();
+    formAddNewTask.reset();
+    const newTask = new Task(task);
+    addNewTask(newTask, id);
+    updateLocalStorage(newTask);
+});
 
-// const deleteTask = (task, id) => {
-//     const tasks = JSON.parse(localStorage.getItem("tasks"));
-//     tasks.splice(id, 1);
-//     localStorage.setItem("tasks", JSON.stringify(tasks));
-//     task.remove();
-// };
+const changeTaskState = (task, id) => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks[id].isCompelted = !(tasks[id].isCompelted);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    task.classList.toggle("completed");
+};
 
-// taskContainer.addEventListener("click", (e) => {
-//    const target  = e.target;
-//    const id = target.parentElement.parentElement.getAttribute("data-id");
-//    if(target.classList.contains("btn-check")){
-//        changeTaskState(target.parentElement.previousElementSibling, id);
-//    }else if(target.classList.contains("btn-delete")){
-//        deleteTask(target.parentElement.parentElement, id);
-//    }
-// });
+const deleteTask = (task, id) => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.splice(id, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    task.remove();
+};
+
+taskContainer.addEventListener("click", (e) => {
+   const target  = e.target;
+   const id = target.parentElement.parentElement.getAttribute("data-id");
+   console.log(target.nextElementSibling, id)
+   if(target.classList.contains("btn-check")){
+    //    need change
+       changeTaskState(target.nextElementSibling, id);
+   }else if(target.classList.contains("btn-delete")){
+    // need change
+       deleteTask(target.parentElement.parentElement, id);
+   }
+});
 
 
-// if(localStorage.tasks === undefined){
-//     localStorage.setItem("tasks", JSON.stringify([]));
-// }else{
-//     JSON.parse(localStorage.tasks).forEach((task, i) => {
-//         addNewTask(task, i);
-//     });
-// }
+if(localStorage.tasks === undefined){
+    localStorage.setItem("tasks", JSON.stringify([]));
+}else{
+    JSON.parse(localStorage.tasks).forEach((task, i) => {
+        addNewTask(task, i);
+    });
+}
